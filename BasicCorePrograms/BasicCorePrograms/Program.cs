ccda2d368867e8185ab3ef898b620c9c621a6c26
vaibhavbohari <vaibhavbohari@gmail.com>
2022-03-13 21:51:40 +0530

    Uc1: Print The Percentage Head and Tail

--- BasicCorePrograms/BasicCorePrograms/Program.cs
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Flip The Coin!");
+Console.WriteLine("Welcome to Coin Flip!");
+
+double head = 0, tail = 0;
+Console.WriteLine("Enter the number of the coin is tossed");
+int repeat = Convert.ToInt32(Console.ReadLine());
+Random random = new Random();
+for (int i = 0; i < repeat; i++)
+{
+
+    double rand = random.NextDouble();
+    Console.WriteLine(rand + " ");
+    if (rand < 0.5) tail++;
+    else head++;
+
+}
+double headpercentage = (head / repeat) * 100;
+Console.WriteLine("Percentage of getting head is :" + headpercentage + "\n");  // \n is prints new line.
+Console.WriteLine("Percentage of getting tail is :" + (100 - headpercentage) + "\n");
+Console.ReadLine();
