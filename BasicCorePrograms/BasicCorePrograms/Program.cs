efc1624cc0ff512c6c999547ce18c8e70b93e17b
vaibhavbohari <vaibhavbohari@gmail.com>
2022-03-14 14:23:39 +0530

    Uc8: Number Is Even Or Odd

--- BasicCorePrograms/BasicCorePrograms/Program.cs
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -1,22 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(" Welcome to Swap To Numbers   ");
+Console.WriteLine(" Welcome to Even Or Odd   ");
 
+int i;
+Console.Write("Enter a Number : ");
+i =Convert.ToInt32(Console.ReadLine());
+
+if (i % 2 == 0)
 {
-    int number_One,
-    number_Two,
-    temp;
-
-    Console.Write("\nEnter the First Number : ");
-    number_One = Convert.ToInt32(Console.ReadLine());
-
-    Console.Write("\nEnter the Second Number : ");
-    number_Two = Convert.ToInt32(Console.ReadLine());
-
-    temp = number_One;
-    number_One = number_Two;
-    number_Two = temp;
-    Console.Write("\nAfter Swapping : ");
-    Console.Write("\nFirst Number : " + number_One);
-    Console.Write("\nSecond Number : " + number_Two);
-    Console.ReadLine();
+    Console.Write("Entered Number is an Even Number");
+    Console.Read();
 }
+else
+{
+    Console.Write("Entered Number is an Odd Number");
+    Console.Read();
+}
+
+
+
+
+
+    
