a9584bdb64742cc1b74ee98a22948d70150b65d9
vaibhavbohari <vaibhavbohari@gmail.com>
2022-03-14 12:23:42 +0530

    Uc : calculate power of 2

--- BasicCorePrograms/BasicCorePrograms/Program.cs
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -1,26 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to LeapYear!");
+Console.WriteLine("Power of 2!");
 
-int i;
-Console.WriteLine("Enter a year:");
-i = Convert.ToInt32(Console.ReadLine());
-if ((i % 400) == 0)
-{
-    Console.WriteLine("Enter year is a Leap yaer:");
-
-}
-else if ((i % 100) == 0)
-{
-    Console.WriteLine("Enter year is a Leap yaer:");
-
-}
-else if ((i % 4) == 0)
-
-{
-    Console.WriteLine("Enter year is a Leap yaer:");
-
-}
-else
-{
-    Console.WriteLine("Enter year is a Not Leap year:");
-}
+    int power, result = 1;
+    string N;
+    Console.WriteLine("Enter the power of 2 : ");
+power = Convert.ToInt32 (Console.ReadLine());
+  //  power = Convert.ToInt32(N);
+    if (power <31 && power >= 0)
+    {
+        while (power != 0)
+        {
+            result = result * 2;
+            --power;
+        }
+        Console.WriteLine("Answer = " + result);
+    }
+    else
+    {
+        Console.WriteLine("Wrong input will over flow int");
+    }
